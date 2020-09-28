6d78e173a103ad45a9b6c0c07852420952228ff4
alsami <al.khatib.sami1990@gmail.com>
2020-09-28 09:02:20 +0200

    chore: scaffold main- and test-project

---
+++ test/IdentityServer4.Contrib.Administration.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace IdentityServer4.Contrib.Administration.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
